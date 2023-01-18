d6f83afda2b384c0460af1b7b65844a9e3863446
Askaniy <askaniy1625@gmail.com>
2023-01-18 12:14:50 +0300

    File reorganization

--- etc/bowring.py
+++ etc/bowring.py
@@ -1,5 +1,8 @@
 from math import sin, cos, tan, atan2, sqrt, hypot, degrees
 
+
+# Sample implementation of the Bowring algorithm
+
 def bowring(x, y, z, a, b):
     long = atan2(y, x)
     p = hypot(x, y)
@@ -7,8 +10,10 @@ def bowring(x, y, z, a, b):
     e2_2 = (a*a - b*b) / (b*b)
 
     beta = a / b * z / p
+    phi, phi0 = None, 0
 
-    for i in range(2):
+    while phi0 != phi:
+        phi0 = phi
         phi = atan2(z + b * e2_2 * sin(beta)**3, p - a * e1_2 * cos(beta)**3)
         beta = atan2(b * tan(phi), a)
         print(phi, beta)
@@ -17,5 +22,6 @@ def bowring(x, y, z, a, b):
     return long, phi, h
 
 
-long, lat, hight = bowring(60, 60, 60, 66.854, 71.492)
+long, lat, hight = bowring(60, 60, 60, 66.854, 71.492) # parameters of Jupiter
+
 print(f'long = {degrees(long)} deg, lat = {degrees(lat)} deg, hight = {hight} km')
